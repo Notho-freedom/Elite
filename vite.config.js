3276eccf6ad76f86602728ae1320f73dfd17e103
Notho-freedom <bobymomo6@gmail.com>
2025-08-07 02:47:11 +0300

    chore: mettre à jour les scripts Capacitor et les dépendances Gradle

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Accepte toutes les connexions
+    host: true,
     cors: {
-      origin: '*', // Permet toutes les origines
+      origin: '*',
       credentials: true
     }
   },
@@ -14,7 +14,7 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'esbuild',
+    minify: 'esbuild'
   },
   base: './'
 })
